ecfd483b2975ae81e4e784d8eafb342cc89eb545
Kidboo10 <atef.sendesni@yh.nackademin.se>
2019-11-14 10:56:08 +0100

    uppdaterat client sever protocoll och question

--- src/Client.java
+++ src/Client.java
@@ -1,48 +1,94 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 
-public class Client {
-
-    String hostName = "172.20.201.252";
-    int portNumber = 44444;
-
+public class Client extends JFrame implements Runnable, ActionListener {
+    Protocoll pro = new Protocoll();
+    JPanel panel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JTextArea area = new JTextArea();
+    Color colorbutton = new Color(30, 29, 37);
+    Color colorarea = new Color(13, 199, 253);
+    int toPort = 12345;
+    String hostName = "172.20.201.51";
+    Socket socket = new Socket(hostName, toPort);
+    Thread thread = new Thread(this);
+    private static JButton[] buttons = new JButton[4];
 
     public Client() throws IOException {
-        try (
-                Socket socket = new Socket(hostName, portNumber);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        ) {
 
-            String fromServer;
-            String fromUser;
+        Server chatServer = new Server();
+        setSize(400, 439);
+        add(panel);
+        panel.setSize(400, 400);
+        panel.setLayout(new BorderLayout());
+        panel.add(area, BorderLayout.CENTER);
+        area.setEditable(false);
+        area.setLineWrap(true);
+        buttonPanel.setLayout(new GridLayout(2, 2));
+        buttonPanel.setSize(400, 200);
+        panel.add(addButtons(), BorderLayout.SOUTH);
+        area.setBackground(colorarea);
 
-            while ((fromServer = in.readLine()) != null) {
+        setLocationRelativeTo(null);
+        area.setVisible(true);
+        buttonPanel.setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        this.thread.start();
 
-                System.out.println("Server: " + fromServer);
-                if (fromServer.equalsIgnoreCase("Bye"))
-                    break;
 
-                fromUser = stdIn.readLine();
-                if (fromUser != null) {
-                    System.out.println("Client: " + fromUser);
-                    out.println(fromUser);
-                }
-            }
+    }
 
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
-            System.exit(1);
+    public JPanel addButtons(){
+        JPanel grid = new JPanel();
+        buttons = new JButton[4];
+        for (int i = 0; i < 4 ; i++) {
+            buttons[i] = new JButton();
+            grid.setLayout(new GridLayout(2,2));
+            buttons[i].setPreferredSize(new Dimension(500,100));
+            buttons[i].addActionListener(this);
+            buttons[i].setFocusable(false);
+            buttons[i].setBackground(colorbutton);
+            buttons[i].setText(pro.getInfo(pro.answers,i));
+            grid.add(buttons[i]);
         }
+        return grid;
+    }
+    public String getRiddle(String[] riddles, int i){
+        return riddles[i];
     }
 
     public static void main(String[] args) throws IOException {
-        Client c = new Client();
+        Client cc = new Client();
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(this.socket.getInputStream()));
+        ) {
+            String message;
+            while ((message = in.readLine()) != null) {
+                area.append(message + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < 4 ; i++) {
+            if(buttons[i] == e.getSource()){
+                if(buttons[i].getText().equalsIgnoreCase("")) {
+                    buttons[i].setBackground(Color.GREEN);
+                }
+            }
+
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,46 @@
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+    int portNumber = 12345;
+
+    public Server() throws IOException {
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine, outPutLine;
+
+            //Initiate conversation with client
+            Protocoll prot = new Protocoll();
+            outPutLine = prot.processInput(null);
+            out.println(outPutLine);
+
+            while ((inputLine = in.readLine()) != null){
+                outPutLine = prot.processInput(inputLine);
+                out.println(outPutLine);
+                if (outPutLine.equalsIgnoreCase("Bye"))
+                    break;
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+    public static void main(String[] args) throws IOException {
+        Server s = new Server();
+    }
 }
